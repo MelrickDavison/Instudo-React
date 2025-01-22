938246e4077417816945c78ef2c2437052676918
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2025-01-22 09:39:12 -0300

    Criando componentes de serviços

---
+++ src/objects/cursos.js
@@ -0,0 +1,24 @@
+class Cursos {
+    cursos = [
+        {id: 1, nome: 'Programação Móvel', area: 'Tecnologia'},
+        {id: 2, nome: 'Física', area: 'Natureza'},
+        {id: 3, nome: 'Matemática', area: 'Natureza'},
+        {id: 4, nome: 'Direito Penal', area: 'Direito'},
+        {id: 5, nome: 'Direito Constitucional', area: 'Direito'},
+        {id: 6, nome: 'ADS', area: 'Tecnologia'},
+        {id: 7, nome: 'Circuitos Elétricos', area: 'Engenharia Elétrica'},
+        {id: 8, nome: 'Filosofia', area: 'Humanas'}
+    ];
+
+    getCursoById(id) {
+        return this.cursos.find(curso => curso.id === parseInt(id));
+    }
+
+    getCursos() {
+        return this.cursos;
+    }
+}
+
+const cursos = new Cursos();
+
+export default cursos;
