44db0c254b34b97a1261e65e06f0a9b49192c5e3
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2025-01-30 11:32:37 -0300

    Melhorando lógica getDoc

--- src/components/cursoDetalhe.js
+++ src/components/cursoDetalhe.js
@@ -1,25 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { collection, getDocs} from 'firebase/firestore'
+import { doc, getDoc} from 'firebase/firestore'
 import { db } from '../services/firebase.config'
 
 const CursoDetalhe = () => {
   const { id } = useParams();
-  const [cursos, setCursos] = useState([]);
-  const collectionRef = collection(db, 'cursos');
-  const [cursoEncontrado, setCursoEncontrado] = useState([]);
+  const [cursoEncontrado, setCursoEncontrado] = useState('');
   
   useEffect(() => {
     const fetchCurso = async () => {
-      await getDocs(collectionRef).then((cursos) => {
-
-        let cursosData = cursos.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-        setCursoEncontrado(cursosData.find(curso => curso.id == id))
-        setCursos(cursosData);
-        }).catch((err) => {
-          console.log(err);
-        })
-
+      console.log(id)
+     const  cursoRef =  doc(db, 'cursos', id)
+     const getCurso = await getDoc(cursoRef)
+     setCursoEncontrado(getCurso.data())
       }
 
     fetchCurso()
@@ -32,9 +25,9 @@ const CursoDetalhe = () => {
         <div className="row">
           <div className="col-md-8">
            
-            <h2>{cursoEncontrado?.nome}</h2>
+            <h2>{cursoEncontrado.nome}</h2>
            
-            <p>Área: {cursoEncontrado?.area}</p>
+            <p>Área: {cursoEncontrado.area}</p>
           
           </div>
           
